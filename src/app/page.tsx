914fb253df649e42632dfb14e3f017d21fb01292
lucidmach <lucidmach@gmail.com>
2023-05-19 15:33:07 +0530

    components inside the app route

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ScrollVideo from "@/app/scrollvideo";
+
+export default function Home() {
+  return (
+    <main>
+      <ScrollVideo dir="/desktop" max={25} />
+    </main>
+  );
+}
