c749c96652ff992aa3e7c119d1c4b9fc1548fe78
lucidmach <lucidmach@gmail.com>
2023-05-23 18:12:03 +0530

    nav menu

---
+++ src/app/menu.tsx
@@ -0,0 +1,67 @@
+import clsx from "clsx";
+
+interface props {
+  currentRoute: string;
+}
+
+interface routeType {
+  name: string;
+  route: string;
+}
+
+const routes: routeType[] = [
+  {
+    name: "Home",
+    route: "/",
+  },
+  {
+    name: "About Us",
+    route: "/about-us",
+  },
+  {
+    name: "Careers",
+    route: "/careers",
+  },
+  {
+    name: "Workshops",
+    route: "/workshop",
+  },
+];
+
+const Menu: React.FC<props> = ({ currentRoute }) => {
+  return (
+    <div className="flex flex-col gap-5 items-end group">
+      <button>
+        <div className="bg-white w-6 h-6 absolute top-9 right-9"></div>
+        <svg
+          className="transition ease-in-out delay-150 h-12 fill-orange-600 rotate-90 hover:fill-orange-500 group-hover:rotate-0"
+          clipRule="evenodd"
+          fillRule="evenodd"
+          strokeLinejoin="round"
+          strokeMiterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m12.002 2c5.518 0 9.998 4.48 9.998 9.998 0 5.517-4.48 9.997-9.998 9.997-5.517 0-9.997-4.48-9.997-9.997 0-5.518 4.48-9.998 9.997-9.998zm2.498 9.995c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25zm-3.75 0c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25zm-3.75 0c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25z" />
+        </svg>
+      </button>
+      <div className="transition ease-in-out delay-150 flex flex-col gap-3 items-end translate-x-32 group-hover:translate-x-0">
+        {routes.map((route) => (
+          <button
+            key={route.name}
+            className={clsx(
+              "bg-orange-600 px-3 py-1 rounded-full text-sm w-fit hover:bg-orange-400",
+              route.route === currentRoute && "bg-orange-500"
+            )}
+            // className={
+            // }
+          >
+            {route.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
--- src/app/overlay.tsx
+++ src/app/overlay.tsx
@@ -1,29 +1,18 @@
 import Image from "next/image";
+import Menu from "./menu";
 
 const OverLay: React.FC = () => {
   return (
-    <div className="w-full h-full fixed top-0 flex flex-col justify-between p-3">
+    <div className="w-full h-full fixed top-0 flex flex-col justify-between p-6">
       <div className="flex flex-row justify-between">
         <Image
-          className="bg-orange-600 px-3 py-2 rounded-full"
+          className="bg-orange-600 h-fit px-3 py-2 rounded-full"
           src={"/logo.webp"}
           height={36}
           width={100}
           alt="logo"
         />
-        <button>
-          <svg
-            className="transition ease-in-out delay-150 fill-orange-600 h-full rotate-90 hover:fill-orange-500 hover:rotate-0"
-            clipRule="evenodd"
-            fillRule="evenodd"
-            strokeLinejoin="round"
-            strokeMiterlimit="2"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="m12.002 2c5.518 0 9.998 4.48 9.998 9.998 0 5.517-4.48 9.997-9.998 9.997-5.517 0-9.997-4.48-9.997-9.997 0-5.518 4.48-9.998 9.997-9.998zm2.498 9.995c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25zm-3.75 0c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25zm-3.75 0c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25z" />
-          </svg>
-        </button>
+        <Menu currentRoute="/" />
       </div>
       <button className="bg-orange-600 px-6 py-2 rounded-full w-fit self-center font-semibold hover:bg-orange-400">
         REGISTER YOUR INTEREST
