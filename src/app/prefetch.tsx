83e3e35368c30af8a380b43870a7518daa94b63a
lucidmach <lucidmach@gmail.com>
2023-05-30 16:20:02 +0530

    frame request int + string error fix

--- src/app/prefetch.tsx
+++ src/app/prefetch.tsx
@@ -8,16 +8,16 @@ interface props {
 const PreFetch: React.FC<props> = ({ path, frames }) => {
   const frameElements = [];
 
-  for (let i = 0; i < frames; i++) {
+  for (let i = 1; i <= frames; i++) {
     frameElements.push(
       <Image
         key={i}
         height={1000}
         width={1000}
         alt="invisible image"
-        className="invisible"
+        className="invisible w-full h-full"
         loading="eager"
-        src={path + "/image" + i + 1 + ".jpg"}
+        src={path + "/image" + i + ".jpg"}
       />
     );
   }
