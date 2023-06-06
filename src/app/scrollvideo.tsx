fa650efabe11642a4b51146cf3a0aa3a11c78cca
lucidmach <lucidmach@gmail.com>
2023-06-06 14:23:20 +0530

    overlay fix

--- src/app/scrollvideo.tsx
+++ src/app/scrollvideo.tsx
@@ -20,6 +20,19 @@ const ScrollVideo: React.FC<props> = ({ dir, max }) => {
     "mobile"
   );
 
+  function requestFullScreen(element: any) {
+    // Supports most browsers and their versions.
+    var requestMethod =
+      element.requestFullScreen ||
+      element.webkitRequestFullScreen ||
+      element.mozRequestFullScreen ||
+      element.msRequestFullScreen;
+
+    if (requestMethod) {
+      requestMethod.call(element);
+    }
+  }
+
   useEffect(() => {
     const onPageLoad = async () => {
       setLoaded(true);
@@ -72,25 +85,31 @@ const ScrollVideo: React.FC<props> = ({ dir, max }) => {
         frames={max}
       />
       <div>
-        <div className="fixed bottom-20 z-20 w-full flex flex-col justify-center items-center ">
-          <svg
-            className="fill-orange-600 animate-bounce"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2c3.309 0 6 2.691 6 6v8c0 3.309-2.691 6-6 6s-6-2.691-6-6v-8c0-3.309 2.691-6 6-6zm0-2c-4.418 0-8 3.582-8 8v8c0 4.418 3.582 8 8 8s8-3.582 8-8v-8c0-4.418-3.582-8-8-8zm0 9c-.829 0-1.5-.672-1.5-1.5s.671-1.5 1.5-1.5 1.5.672 1.5 1.5-.671 1.5-1.5 1.5z" />
-          </svg>
-          <p className="text-orange-600">scroll down</p>
+        <div className="fixed bottom-20 p-4 z-20 w-full flex flex-col justify-center items-center ">
+          <div className="flex flex-col items-center bg-black p-4 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30">
+            <svg
+              className="fill-orange-600 animate-bounce"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2c3.309 0 6 2.691 6 6v8c0 3.309-2.691 6-6 6s-6-2.691-6-6v-8c0-3.309 2.691-6 6-6zm0-2c-4.418 0-8 3.582-8 8v8c0 4.418 3.582 8 8 8s8-3.582 8-8v-8c0-4.418-3.582-8-8-8zm0 9c-.829 0-1.5-.672-1.5-1.5s.671-1.5 1.5-1.5 1.5.672 1.5 1.5-.671 1.5-1.5 1.5z" />
+            </svg>
+            <p className="text-orange-600">scroll down</p>
+          </div>
         </div>
         <Image
-          className="w-full h-full fixed top-0"
-          style={{ width: w.current, height: h.current }}
+          className="fixed bottom-0"
+          style={{ width: "100vw", height: "100vh" }}
           width={1000}
           height={1000}
           src={dir + "/" + orientation + "/image" + frameNumber + ".jpg"}
           alt="current frame in the video"
+          onClick={() => {
+            var elem = document.body; // Make the body go full screen.
+            requestFullScreen(elem);
+          }}
           priority
         />
       </div>
